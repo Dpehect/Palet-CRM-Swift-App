741a3b46adfc3f84d9d2fa76b35d8bf9a337141a
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-06-25 16:29:44 +0300

    Initial Commit

---
+++ Palet Crm/Palet_CrmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Palet_CrmApp.swift
+//  Palet Crm
+//
+//  Created by Yunus Emre Gürlek on 25.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Palet_CrmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Palet CrmTests/Palet_CrmTests.swift
@@ -0,0 +1,18 @@
+//
+//  Palet_CrmTests.swift
+//  Palet CrmTests
+//
+//  Created by Yunus Emre Gürlek on 25.06.2026.
+//
+
+import Testing
+
+struct Palet_CrmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
